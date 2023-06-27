bbebd48465008adbd9b42b4d8b923667477e2f7a
nobilitat <gusejnganbarov08@gmail.com>
2023-06-28 06:43:40 +1000

    vercel config

--- fake_recognition/config/settings.py
+++ fake_recognition/config/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-&5$&rkm5-+jk)uj-r=^oid#=o#3fuddks$b0$8if&5wi2@bc68
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app'
+]
 
 
 # Application definition
